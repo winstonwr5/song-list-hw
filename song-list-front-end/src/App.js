ef09bf787f7cd1695611563da5c9d3b5d3bad0d1
William Winston <winston.william@gmail.com>
2020-03-13 17:49:50 -0400

    front end basics

--- song-list-front-end/src/App.js
+++ song-list-front-end/src/App.js
@@ -1,8 +1,24 @@
 import React from 'react'
+let baseURL = process.env.REACT_APP_BASEURL
+
+if (process.env.NODE_ENV === 'development') {
+  baseURL = 'http://localhost:3003'
+} else {
+  baseURL = 'https://localhost:3003'
+}
 
 class App extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            songs: [],
+
+        }
+    }
     render() {
-        return <div><h1>HI!</h1></div>
+        return ()
+            <div className='container'>
+                <h1>SONGS</h1>
     }
 }
 
