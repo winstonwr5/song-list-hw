96d3fc6c10782bb660d13c43150d2f49fc6e4fe0
Chris Bellaro <chrisbellaro@Chriss-Air.home>
2020-03-15 18:39:58 -0400

    fixed merge conflicts

--- song-list-front-end/src/App.js
+++ song-list-front-end/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import NewForm from './components/NewForm.js'
+import './css/materialize.min.css'
+// import NewForm from './components/NewForm.js'
 import Show from './components/Show.js'
 let baseURL = process.env.REACT_APP_BASEURL
 
@@ -14,26 +15,26 @@ class App extends React.Component {
         super(props)
         this.state = {
             songs: [],
+            Playlist: null
         }
         this.getSongs = this.getSongs.bind(this)
         this.handleAddSong = this.handleAddSong.bind(this)
         this.deleteSong = this.deleteSong.bind(this)
-        this.toggleLiked = this.toggleLiked.bind(this)
+        this.togglePlaylist = this.togglePlaylist.bind(this)
         this.getSong = this.getSong.bind(this)
     }
     componentDidMount(){
-        this.getSongs()
-    }
-
-    async getSongs (){
-        try{
-            let response = await fetch(`${baseURL}/songs`)
-            let data = await response.json()
-            this.setState({songs: data})
-        } catch (e) {
-            console.error(e)
-        }
-    }
+       this.getSongs()
+   }
+   async getSongs() {
+       try{
+           let response = await fetch(`${baseURL}/songs`)
+           let data = await response.json()
+           this.setState({songs: data})
+       } catch(error){
+           console.error(error)
+       }
+   }
     handleAddSong(song) {
         const copySongs = [song, ...this.state.songs]
         this.setState({
@@ -65,28 +66,26 @@ class App extends React.Component {
          console.error(e);
      }
      }
-     async toggleOnPlaylist (song) {
-         console.log(song)
-         try{
-             let response = await fetch(baseURL + '/songs/' + song._id, {
-                 method: 'PUT',
-                 body: JSON.stringify({onPlaylist:
-                 !song.onPlaylist}),
-                 headers: {
-                     'Content-Type': 'application/json'
-                 }
-             })
-             let updatedSong = await response.json()
-             const foundSong = this.state.songs.findIndex(foundItem =>
-             foundItem._id === song._id)
-             const copySongs = [...this.state.songs]
-             copySongs[foundSong].onPlaylist = updatedSong.onPlaylist
-             console.log(updatedSong)
-             this.setState({songs: copySongs})
-        }catch(e){
-            console.error(e)
-         }
-     }
+     async togglePlaylist (song){
+        console.log(song.Playlist);
+        try{
+            let response = await fetch(baseURL + '/songs/' + song._id, {
+                method: 'PUT',
+                body: JSON.stringify({Playlist: !song.Playlist}),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            })
+            let updatedSong = await response.json()
+            const foundSong = this.state.songs.findIndex(foundItem => foundItem._id === song._id)
+            const copySongs = [...this.state.songs]
+            copySongs[foundSong].Playlist = updatedSong.Playlist
+            console.log(updatedSong)
+            this.setState({songs: copySongs})
+        }catch(error){
+            console.error(error)
+        }
+    }
      getSong(song) {
          this.setState({song: song})
          console.log(song)
@@ -95,31 +94,25 @@ class App extends React.Component {
         return (
             <div className='container'>
                 <h1>SONGS</h1>
-                <NewForm
-                handleAddSong={this.handleAddSong}
-                baseURL={baseURL} />
-
-            <table>
-                <tbody>
-                    {this.state.songs.map(song => {
-                        return (
-                            <tr key={song._id} onMouseOver={() =>
-                                this.getSong(song)}>
-                            <td onDoubleClick={() =>
-                                this.toggleOnPlaylist(song)}
-                            className={song.onPlaylist
-                                ? 'On Playlist'
-                                : null}>
-                                {song.name} :
-                                {song.onPlaylist ? 'on your list' : 'not on your list'}</td>
-                            <td onClick ={() => {
-                                this.deleteSong(song._id)}}>X</td>
-                            </tr>
-                        )
-                    })
-                }
-                </tbody>
-            </table>
+                <table className="highlight">
+                   <tbody>
+                     { this.state.songs.map(song => {
+                         return (
+                             <tr key={song._id}>
+                             <td>Song: {song.Song}</td>
+                             <td>Album: {song.Album}</td>
+                             <td>Artist: {song.Artist}</td>
+                             <td>Released: {song.Released}</td>
+                             <td>Likes: {song.Likes}</td>
+                             <td  onDoubleClick={() => this.togglePlaylist(song)}>
+                                 {song.Playlist ? 'On playlist': 'Not on playlist' }
+                             </td>
+                             </tr>
+                         )
+                     })
+                 }
+                 </tbody>
+                 </table>
             </div>
         )
     }
