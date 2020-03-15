606d21ad8b9138575422a4998eee02a139bf516b
William Winston <winston.william@gmail.com>
2020-03-15 17:03:43 -0400

    App.js and delete route front end work

--- song-list-front-end/src/App.js
+++ song-list-front-end/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import 'materialize-css/dist/css/materialize.min.css'
 let baseURL = process.env.REACT_APP_BASEURL
 
 if (process.env.NODE_ENV === 'development') {
@@ -15,10 +16,57 @@ class App extends React.Component {
 
         }
     }
+    async deleteSong (id){
+         console.log('I made a delete request to here:', `${baseURL}/songs/${id}`)
+         try{
+         let response = await fetch(baseURL + '/songs/' + id, {
+             method: 'DELETE'
+         })
+         let data = await response.json()
+         const foundSong =
+           this.state.songs.findIndex(song =>
+           song._id === id)
+           const copySongs =
+           //spread operator copies the array and prevents you from mutating state. this is making a brand new array - and passing by REFERENCE to the original array
+            [...this.state.songs]
+           //this BELOW will find the holiday and splice it out of the copyHolidays array
+           copySongs.splice(foundSong, 1)
+           this.setState({songs: copySongs})
+     } catch(e){
+         console.error(e);
+     }
+     }
     render() {
-        return ()
+        return (
             <div className='container'>
                 <h1>SONGS</h1>
+                <NewForm
+                handleAddSong={this.handleAddSong}
+                baseURL={baseURL} />
+
+            <table>
+                <tbody>
+                    {this.state.songs.map(song => {
+                        return (
+                            <tr key={song._id} onMouseOver={() =>
+                                this.getSong(song)}>
+                            <td onDoubleClick={() =>
+                                this.toggleLiked(song)}
+                            className={song.liked
+                                ? 'liked'
+                                : null}>
+                                {song.name}
+                            }}
+                            <td onClick ={() => {
+                                this.deleteSong(song._id)}}>X</td>
+                            </tr>
+                        )
+                    })
+                }
+                        </tbody>
+            </table>
+            </div>
+        )
     }
 }
 
